09090044155fb3072148258d57b33bdd6c0fa82c
Blackhole1504 <51298403+Blackhole1504@users.noreply.github.com>
2024-06-27 20:18:18 +0200

    fix2 selection hors stackedchart

--- scripts/stackedchart.js
+++ scripts/stackedchart.js
@@ -4,7 +4,7 @@ function drawStackedChart() {
     // Taille et marges du graphique
     const margin = { top: 20, right: 80, bottom: 50, left: 50 },
           width = document.getElementById('stackedchart').clientWidth - margin.left - margin.right,
-          height = 400 - margin.top - margin.bottom; 
+          height = 600 - margin.top - margin.bottom; 
 
     // Création du SVG
     const svg = d3.select("#stackedchart").append("svg")
@@ -52,93 +52,96 @@ function drawStackedChart() {
         svg.append("g")
             .call(d3.axisLeft(y));
 
-        // Ajouter un rectangle en fond pour couvrir la zone sans barres
-        const fond = svg.append("rect")
-            .attr("x", 0)
-            .attr("y", 0)
-            .attr("width", width)
-            .attr("height", height)
-            .attr("fill", "rgba(0, 0, 0, 0)")
-            .on("click", function (event) {
-                if (event.target.tagName !== 'rect') { // Si on clique en dehors des barres, réinitialise l'opacité et le texte
-                    svg.selectAll("rect")
-                        .style("opacity", 1);
-                    d3.select("#blabla1").remove();
-                    d3.select("#blabla2").remove();
-                }
-            });
+      // Ajouter un rectangle en fond pour couvrir la zone sans barres
+      const fond = svg.append("rect")
+      .attr("x", 0)
+      .attr("y", 0)
+      .attr("width", width)
+      .attr("height", height)
+      .attr("fill", "rgba(0, 0, 0, 0)")
+      .on("click", function (event) {
+          if (event.target === this) {
+              svg.selectAll("rect.bar")
+                  .style("opacity", 1);
+              d3.select("#blabla1").remove();
+              d3.select("#blabla2").remove();
+          }
+      });
 
-        // Création des barres
-        const bars = svg.append("g")
-            .selectAll("g")
-            .data(stackedData)
-            .enter().append("g")
-            .style("fill", (d, i) => customColors[i % customColors.length])
-            .selectAll("rect")
-            .data(d => d)
-            .enter().append("rect")
-            .attr("x", d => x(d.data.Date))
-            .attr("y", d => y(d[1]))
-            .attr("height", d => y(d[0]) - y(d[1]))
-            .attr("width", x.bandwidth())
-            .on("mouseover", function (event, d) {
-                var date = d.data.Date;
-                var mouseX = d3.pointer(event, this)[0];
-                var mouseY = d3.pointer(event, this)[1];
+    // Création des barres
+    const bars = svg.append("g")
+        .selectAll("g")
+        .data(stackedData)
+        .enter().append("g")
+        .style("fill", (d, i) => customColors[i % customColors.length])
+        .selectAll("rect")
+        .data(d => d)
+        .enter().append("rect")
+        .attr("class", "bar") 
+        .attr("x", d => x(d.data.Date))
+        .attr("y", d => y(d[1]))
+        .attr("height", d => y(d[0]) - y(d[1]))
+        .attr("width", x.bandwidth())
+        .on("mouseover", function (event, d) {
+            var date = d.data.Date;
+            var mouseX = d3.pointer(event, this)[0];
+            var mouseY = d3.pointer(event, this)[1];
 
-                // Afficher le texte et le positionner sous le curseur
-                svg.append("text")
-                    .attr("id", "tooltip")
-                    .attr("x", mouseX + 20) // Position horizontale du curseur corrigée pour etre au bon endroit
-                    .attr("y", mouseY + 35) // Position horizontale du curseur corrigée pour etre au bon endroit
-                    .attr("text-anchor", "middle")
-                    .attr("font-family", "sans-serif")
-                    .attr("font-size", "15px")
-                    .attr("font-weight", "bold")
-                    .attr("fill", "rgba(210, 210, 210)")
-                    .text(date);
-            })
-            .on("mouseout", function () {
-                d3.select("#tooltip").remove();
-            })
-            .on("click", function (event, d) {
-                // Ajuster l'opacité des barres
-                svg.selectAll("rect")
-                    .style("opacity", 0.5); // Réduire l'opacité de toutes les barres
-                d3.select(this)
-                    .style("opacity", 1); // Rétablir l'opacité de la barre cliquée
-                d3.select("#blabla1").remove();
-                d3.select("#blabla2").remove();
+            // Afficher le texte et le positionner sous le curseur
+            svg.append("text")
+                .attr("id", "tooltip")
+                .attr("x", mouseX + 20) // Position horizontale du curseur corrigée pour etre au bon endroit
+                .attr("y", mouseY + 35) // Position horizontale du curseur corrigée pour etre au bon endroit
+                .attr("text-anchor", "middle")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "15px")
+                .attr("font-weight", "bold")
+                .attr("fill", "rgba(210, 210, 210)")
+                .text(date);
+        })
 
-                const date = d.data.Date;
-                const value = d[1] - d[0]; // Calculer la valeur de la barre
+        .on("mouseout", function () {
+            d3.select("#tooltip").remove();
+        })
+        
+        .on("click", function (event, d) {
+            // Ajuster l'opacité des barres
+            svg.selectAll("rect")
+                .style("opacity", 0.5); // Réduire l'opacité de toutes les barres
+            d3.select(this)
+                .style("opacity", 1); // Rétablir l'opacité de la barre cliquée
+            d3.select("#blabla1").remove();
+            d3.select("#blabla2").remove();
 
-                const xPosition = x(date) + x.bandwidth() / 2;
-                const yPosition = y(d[1]);
-                // Affichage de la date
-                svg.append("text")
-                    .attr("id", "blabla1")
-                    .attr("x", xPosition)
-                    .attr("y", yPosition - 20) // Position verticale pour la date
-                    .attr("text-anchor", "middle")
-                    .attr("font-family", "sans-serif")
-                    .attr("font-size", "15px")
-                    .attr("font-weight", "bold")
-                    .attr("fill", "rgba(210, 210, 210)")
-                    .text(`Date: ${date}`);
+            const date = d.data.Date;
+            const value = d[1] - d[0]; // Calculer la valeur de la barre
 
-                // Affichage de la valeur
-                svg.append("text")
-                    .attr("id", "blabla2")
-                    .attr("x", xPosition)
-                    .attr("y", yPosition - 5) // Position verticale pour la valeur
-                    .attr("text-anchor", "middle")
-                    .attr("font-family", "sans-serif")
-                    .attr("font-size", "15px")
-                    .attr("font-weight", "bold")
-                    .attr("fill", "rgba(210, 210, 210)")
-                    .text(`Valeur: ${value}`);
-            });
+            const xPosition = x(date) + x.bandwidth() / 2;
+            const yPosition = y(d[1]);
+            // Affichage de la date
+            svg.append("text")
+                .attr("id", "blabla1")
+                .attr("x", xPosition)
+                .attr("y", yPosition - 20) // Position verticale pour la date
+                .attr("text-anchor", "middle")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "15px")
+                .attr("font-weight", "bold")
+                .attr("fill", "rgba(210, 210, 210)")
+                .text(`Date: ${date}`);
+
+            // Affichage de la valeur
+            svg.append("text")
+                .attr("id", "blabla2")
+                .attr("x", xPosition)
+                .attr("y", yPosition - 5) // Position verticale pour la valeur
+                .attr("text-anchor", "middle")
+                .attr("font-family", "sans-serif")
+                .attr("font-size", "15px")
+                .attr("font-weight", "bold")
+                .attr("fill", "rgba(210, 210, 210)")
+                .text(`Valeur: ${value}`);
+        });
 
         // Légende
         const legendLabels = ["Hydraulique", "Nucléaire", "Thermique"];
